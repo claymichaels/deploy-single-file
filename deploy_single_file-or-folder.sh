ca580fadd2acfcb8084f31defd25da9a7f3b9ea1
claymichaels <clay.michaels+github@gmail.com>
2017-07-31 22:42:04 -0400

    Initial commit on branch
    Slight modification to send folders in addition to single files. Need to
    add the "does the file exist?" back in, but allowing folders as well.

---
+++ deploy_single_file-or-folder.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+# Deploy a single file to an entire fleet and log progress
+# v1.3 - Clay Michaels 19 July 2016
+#   Made log file output to a relative path of .<logfile> to make it more portable.
+# v1.2.5 - Clay Michaels 15 March 2016
+#   Changed "Silent" flag to "Supress".
+# v1.2.4 - Clay Michaels 2 March 2016
+#   Added colorized output.
+# v1.2.3 - Clay Michaels 29 Feb 2016
+#   Added -s option to skip output for Already Done
+# v1.2.2 - Clay Michaels 25 Feb 2016
+#   Added progress numbers e.g. "4/12 done"
+#   Known bug (found by Jason Chong) - acela.1 and .2 trigger "already done"
+#       Based on acela.1[0-9] and acela.2[0-1]
+#       RESOLVED: changed /etc/hosts to put "acela.ts02" before "acela.2"
+# v1.2.1 - Clay Michaels 23 Feb 2016
+#   updated file paths after moving out of /dev
+#   removed "CCU:" at the beginning of each output line
+# v1.2 - Clay michaels 18 Nov 2015
+#   added -r flag to resume
+#   changed log format to include the fleet,file,location at the top
+# v1.1 - clay michaels
+#   added -p portal flag
+# v1 - clay michaels 12 Oct 2015
+
+colorize() {
+    case $1 in
+        green)
+            echo -e "\e[92m$2\e[0m"
+            ;;
+        red)
+            echo -e "\e[91m$2\e[0m"
+            ;;
+    esac
+}
+
+while getopts ":dsr:" opt
+do
+    case $opt in
+        d) # "Portal" - deploy to portals
+            portal=true
+            shift
+            ;;
+        r) # "Resume" - resume previous deployment
+            resume=true
+            resume_log=$OPTARG
+            ;;
+        s) # "Supress" - skip "Already Done" option
+            supress=true
+            ;;
+        \?)
+            this_script=`basename "$0"`
+            echo "Invalid option: -$OPTARG" >&2
+            echo "Expected one of the following patterns:"
+            echo "$this_script <fleet> <file> <deployment path>"
+            echo "$this_script -r <existing log file to resume>"
+            exit 1
+            ;;
+    esac
+done
+
+#############################
+# Trying to parse fleet, file, location from input
+if [ "$resume" = true ]
+then
+    # resuming
+    if [ -e $resume_log ]
+    then
+        echo "resuming $resume_log"
+        log=$resume_log
+        fleet="`head -n3 $resume_log | sed -n 1p`"
+        file_to_send="`head -n3 $resume_log | sed -n 2p`"
+        location="`head -n3 $resume_log | sed -n 3p`"
+        echo "Fleet    =$fleet"
+        echo "File     =$file_to_send"
+        echo "Location =$location"
+        already_done=`cat $log`
+    else
+        # resuming, but log does not exist
+        echo "Input file does not exist!"
+        exit 1
+    fi
+else
+    # Starting a new deployment (not resuming)
+    echo "Starting new deployment:"
+    fleet=$1
+    file_to_send=$2
+    file_to_send_printable=${file_to_send##*/}
+    location=$3
+    log="${fleet}_${file_to_send_printable}.log"
+    echo "Fleet    =$fleet"
+    echo "File     =$file_to_send"
+    echo "Location =$location"
+    echo "Log file =$log"
+    echo "$fleet" > $log
+    echo "$file_to_send" >> $log
+    echo "$location" >> $log
+    already_done=""
+fi
+
+#if ! [ -f $file_to_send ] # deployment file does not exist
+#then
+#    echo "Whoops! The file \"$file_to_send\" doesn't exist!"
+#    exit 1
+#fi
+
+
+
+
+
+hosts=`cat /etc/hosts | grep $fleet | grep -vi "bench" | grep ^10. | tr -s ' ' | cut -d' ' -f2`
+date=$(date)
+
+success_count=0
+total_count=0
+for host in $hosts
+do
+    ((total_count++))
+    if [[ $already_done =~ $host ]]
+    then
+        if [[ $supress != true ]]
+        then
+            echo "$host - Already done."
+        fi
+        ((success_count++))
+    else
+        response=$(ping -c 1 $host)
+        if [[ $response == *"100% packet loss"* ]]
+        then
+            colorize red "$host - Offline"
+            continue
+        else
+            if [[ $portal == true ]]
+            then
+                output=`rsync --rsh='ssh -p8022' $file_to_send $host:$location 2>&1`
+            else
+                output=`rsync  -a $file_to_send $host:$location 2>&1`
+            fi
+            if ! [ -z "$output" ]
+            then
+                useful_lines=`echo $output | grep -v "bind: Address already in use" | grep -v "channel_setup_fwd_listener: cannot listen to port" | grep -v "Could not request local forwarding"`
+                useful_line_count=`echo $output | grep -v "bind: Address already in use" | grep -v "channel_setup_fwd_listener: cannot listen to port" | grep -v "Could not request local forwarding" | wc -l`
+                if [[ $useful_line_count > 0 ]]
+                then
+                    echo "Error!"
+                    echo $useful_lines
+                    continue
+                fi
+            fi
+            colorize green "$host - Done."
+            echo "$date $host" >> $log
+            ((success_count++))
+        fi
+    fi
+done
+
+echo "Status: $success_count/$total_count"
